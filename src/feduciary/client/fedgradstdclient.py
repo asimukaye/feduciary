normalization bug fix

src/feduciary/client/fedgradstdclient.py
@@ -13,38 +13,13 @@ from torch.nn import Module, Parameter
 from torch.optim import Optimizer
 import torch
 from .baseflowerclient import  BaseFlowerClient, MetricManager, ClientConfig
-from feduciary.strategy.fedstdevstrategy import FedstdevStrategy
-from feduciary.config import FedstdevClientConfig, TrainConfig
+from feduciary.strategy.fedgradstdstrategy import FedgradstdStrategy, FedgradIns
+from feduciary.config import FedgradstdClientConfig
 from feduciary.common.utils import get_time
 from feduciary.common import typing as fed_t
 
 logger = logging.getLogger(__name__)
 
- # NOTE: Multithreading causes atleast 3x slowdown for 2 epoch case. DO not use until necessary
-def train_one_model(model:Module, dataloader: DataLoader, seed: int, cfg: TrainConfig, optim_partial, criterion, mm: MetricManager)->t.Tuple[int, Module,fed_t.Result]:
-    out_result = fed_t.Result()
-    optimizer: Optimizer = optim_partial(model.parameters())
-    model.train()
-    model.to(cfg.device)
-    # iterate over epochs and then on the batches
-    for _epoch in range(cfg.epochs):
-        for inputs, targets in dataloader:
-            inputs, targets = inputs.to(cfg.device), targets.to(cfg.device)
-
-            model.zero_grad(set_to_none=True)
-
-            outputs: Tensor = model(inputs)
-            loss: Tensor = criterion(outputs, targets)
-            loss.backward()
-            optimizer.step()
-
-            # accumulate metrics
-            mm.track(loss.item(), outputs, targets)
-        else:
-            out_result = mm.aggregate(len(
-                list(dataloader.dataset)), _epoch)
-            mm.flush()
-    return (seed, model, out_result)
 
 
 def get_to_nearest_log2(num, quotient):
@@ -61,22 +36,21 @@ def get_to_nearest_log2(num, quotient):
 class ClientInProtocol(t.Protocol):
     server_params: dict
 
-@dataclass
-class ClientOuts:
-    client_params: fed_t.ActorParams_t
-    client_param_stds: fed_t.ActorParams_t
+# @dataclass
+# class ClientOuts:
+#     client_params: fed_t.ActorParams_t
+#     client_grad_mus: fed_t.ActorDeltas_t
+#     client_grad_stds: fed_t.ActorDeltas_t
     
 
-class FedstdevClient(BaseFlowerClient):
+class FedgradstdClient(BaseFlowerClient):
     # TODO: Fix the argument ordering structure
-    def __init__(self, cfg: FedstdevClientConfig, **kwargs):
+    def __init__(self, cfg: FedgradstdClientConfig, **kwargs):
         super().__init__(cfg, **kwargs)
 
         self.cfg = deepcopy(cfg)
         # self._root_dir = f'{self.cfg.metric_cfg.cwd}/temp_json'
         # os.makedirs(self._root_dir, exist_ok=True)
-
-        self.train = self.train_single_thread
 
         # Keep n iters consistent with the iid split
         if cfg.n_iters -1 != len(self.training_set)//self.train_cfg.batch_size:
@@ -96,7 +70,7 @@ class FedstdevClient(BaseFlowerClient):
         for seed, model in self._model_map.items():
             self._optimizer_map[seed] = self.optim_partial(model.parameters())
 
-        self._param_std :fed_t.ActorParams_t  = self._model.state_dict()
+        # self._param_std :fed_t.ActorParams_t  = self._model.state_dict()
         self._grad_mu : fed_t.ActorDeltas_t = {p_key: torch.empty_like(param.data) for p_key, param in self._model.named_parameters()}
 
         # self._empty_grads = deepcopy(self._grad_mu)
@@ -117,29 +91,23 @@ class FedstdevClient(BaseFlowerClient):
                 sampler = RandomSampler(data_source=dataset, generator=gen)
                 self._generator[seed] = gen
                 loader_dict[seed] = DataLoader(dataset=dataset, sampler=sampler, batch_size=self.train_cfg.batch_size)
-            # for i, (inputs, targets) in enumerate(loader_dict[seed]):
-            #     with open(f'{self._root_dir}/{self.cfg.metric_cfg.file_prefix}_targets_pre_{self._round}_{seed}_{i}.json', 'w') as f:
-            #                 json.dump(targets.tolist(), f)
-
 
         return loader_dict
     
     def unpack_train_input(self, client_ins: fed_t.ClientIns) -> ClientInProtocol:
-        specific_ins = FedstdevStrategy.client_receive_strategy(client_ins)
+        specific_ins = FedgradstdStrategy.client_receive_strategy(client_ins)
         return specific_ins
     
     def pack_train_result(self, result: fed_t.Result) -> fed_t.ClientResult1:
         self._model.to('cpu')
-        client_outs = ClientOuts(
+        client_outs = FedgradIns(
                     client_params=self._model.state_dict(),
-                    client_param_stds=self._get_parameter_std_dev()
+                    client_grad_mus=self._get_gradients_average(),
+                    client_grad_stds=self._get_gradients_std_dev()
                 )
-        specific_res = FedstdevStrategy.client_send_strategy(client_outs, result=result)
+        specific_res = FedgradstdStrategy.client_send_strategy(client_outs, result=result)
         return specific_res
  
-
-    def _get_parameter_std_dev(self)->dict[str, Parameter]:
-        return deepcopy(self._param_std)
     
     def _get_gradients_std_dev(self)->dict[str, Tensor]:
         return deepcopy(self._grad_std)
@@ -165,14 +133,17 @@ class FedstdevClient(BaseFlowerClient):
             std_, mean_ = torch.std_mean(stacked, dim=0)
 
             stacked_grad = torch.stack(tmp_grad_list)
-            std_grad_, mean_grad_ = torch.std_mean(stacked_grad, dim=0)
+            if self.cfg.abs_before_mean:
+                std_grad_, mean_grad_ = torch.std_mean(stacked_grad.abs(), dim=0)
+            else:
+                std_grad_, mean_grad_ = torch.std_mean(stacked_grad, dim=0)
 
             param.data.copy_(mean_.data)
             # with get_time():
             param.grad = mean_grad_.data
 
             # STATEFUL FUNCTIONS MAY NOT WORK WITH MULTITHREADING
-            self._param_std[name].data = std_.to('cpu')
+            # self._param_std[name].data = std_.to('cpu')
             self._grad_mu[name] = mean_grad_.to('cpu')
             self._grad_std[name] = std_grad_.to('cpu')
 
@@ -191,7 +162,7 @@ class FedstdevClient(BaseFlowerClient):
         
         return fed_t.Result(metrics=avg_metrics, size=sample_res.size, metadata=sample_res.metadata,event=sample_res.event, phase=sample_res.phase, _round=self._round, actor=self._cid)
     
-    def train_single_thread(self, train_ins: ClientInProtocol) -> fed_t.Result:
+    def train(self, train_ins: ClientInProtocol) -> fed_t.Result:
         # MAYBE THIS PART IS REDUNDANT
         self._model.load_state_dict(train_ins.server_params)
         self._optimizer = self.optim_partial(self._model.parameters())
@@ -229,6 +200,7 @@ class FedstdevClient(BaseFlowerClient):
                     # with open(f'{self._root_dir}/{self.cfg.metric_cfg.file_prefix}_targets_{self._round}_{seed}_{i}.json', 'w') as f:
                     #     json.dump(targets.tolist(), f)
                     # logger.debug(f'CLIENT {self.id} SEED: {seed}, EPOCH: {epoch}, BATCH: {i}')
+
                     inputs, targets = inputs.to(self.train_cfg.device), targets.to(self.train_cfg.device)
 
                     model.zero_grad(set_to_none=True)
@@ -237,10 +209,10 @@ class FedstdevClient(BaseFlowerClient):
                     loss: Tensor = self.criterion(outputs, targets)
                     loss.backward()
                     optimizer.step()
+
                     for p_key, param in model.named_parameters():
                         add_grad = 0 if param.grad is None else param.grad
                         self._cum_gradients_map[seed][p_key] = self._cum_gradients_map[seed].get(p_key, 0) + add_grad # type: ignore
-
       
                     self.metric_mngr.track(loss.item(), outputs, targets)
                 else:
@@ -271,24 +243,5 @@ class FedstdevClient(BaseFlowerClient):
 
         self._optimizer_map = {seed: deepcopy(self._optimizer) for seed in self.cfg.seeds}
 
-   
-    def train_multi_thread(self):
-        # Run an round on the client
-    
-        self.metric_mngr._round = self._round
-        self._model.train()
-        self._model.to(self.train_cfg.device)
-        out_result = fed_t.Result()
-        # for seed, model in self._model_map.items():   
-        with ThreadPoolExecutor(max_workers=len(self._model_map)) as exec:
-            futures = {exec.submit(train_one_model, model, self.train_loader_map[seed], seed, self.train_cfg, self.optim_partial, self.criterion, deepcopy(self.metric_mngr)) for seed, model in self._model_map.items()}
-            for fut in as_completed(futures):
-                seed, model, out_result = fut.result()
-                self._model_map[seed] = model
-        
-        self._compute_average_model_and_std(self._model_map)
-        
 
-        logger.info(f'CLIENT {self.id} Completed update')
-        return out_result
  

src/feduciary/strategy/cgsvstrategy.py
@@ -6,17 +6,18 @@ from torch.nn.utils import parameters_to_vector
 
 from feduciary.config import ClientConfig, CGSVConfig
 from feduciary.results.resultmanager import ClientResult
-from .baseserver import BaseServer, BaseStrategy
+from feduciary.strategy import ABCStrategy
 logger = logging.getLogger(__name__)
 
 
-class CgsvOptimizer(BaseStrategy):
+# FIXME: Complete this
+
+class CgsvStrategy(ABCStrategy):
     def __init__(self, params, client_ids, **kwargs):
         self.gamma = kwargs.get('gamma')
         self.lr = kwargs.get('lr')
 
         defaults = dict(lr=self.lr)
-        super(CgsvOptimizer, self).__init__(params=params, defaults=defaults)
 
         self.alpha = kwargs.get('alpha')
         self.local_grad_norm = None

src/feduciary/strategy/fedstdevstrategy.py
@@ -16,7 +16,7 @@ from feduciary.strategy.basestrategy import random_client_selection
 from feduciary.strategy.fedoptstrategy import gradient_average_update
 ### Define the configurations required for this strategy
 from feduciary.common.utils import generate_client_ids
-
+from enum import Enum
 @dataclass
 class FedstdevCfgProtocol(t.Protocol):
     '''Protocol for Fedstdev strategy config'''
@@ -28,6 +28,13 @@ class FedstdevCfgProtocol(t.Protocol):
     num_clients: int
 
 
+class WeightingStrategy(str, Enum):
+    PARAM_SIGMA_LAYER_WIS = 'param_sigma'
+    PARAM_SIGMA_SCALAR = 'param_sigma_scalar'
+    PARAM_SIGMA_BY_MU_FULL_DIM = 'param_sigma_by_mu_full_dim'
+    PARAM_SIGMA_BY_MU_SCALAR_AVG = 'param_sigma_by_mu_scalar_avg'
+    PARAM_SIGMA_BY_MU_SCALAR_WTD_AVG = 'param_sigma_by_mu_scalar_wtd_avg'
+    PARAM_SIGMA_BY_MU_LAYER_WISE = 'param_sigma_by_mu_layer_wise'
 # Type declarations
 ScalarWeights_t = dict[str, float]
 TensorWeights_t = dict[str, Tensor]
@@ -131,7 +138,6 @@ class FedstdevStrategy(ABCStrategy):
         self._clnt_sigma_by_mu: fed_t.ClientParams_t = {cid: {param: Parameter(torch.empty_like(val.data)) for param, val in self._server_params.items()} for cid in client_ids}
         # tracking client deltas for logging
         self._client_deltas: fed_t.ClientDeltas_t = {cid: {param: torch.empty_like(val.data) for param, val in self._server_params.items()} for cid in client_ids}
-
 
 
     @classmethod
@@ -261,6 +267,7 @@ class FedstdevStrategy(ABCStrategy):
 
         return outs
     
+
     @classmethod
     def _compute_sigma_by_mu(cls, sigmas: fed_t.ActorParams_t, mus: fed_t.ActorParams_t) -> fed_t.ActorParams_t:
         '''Computes sigma/mu for each parameter'''